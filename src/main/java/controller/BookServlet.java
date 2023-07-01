4545d4803a4d35ccb8b4a0d4002f08d9ab62f0bc
HyungSeok Park <dnflekf2748@gmail.com>
2023-07-01 17:22:58 +0900

    각각의 endpoint 분기 처리

--- src/main/java/controller/BookServlet.java
+++ src/main/java/controller/BookServlet.java
@@ -1,6 +1,5 @@
 package controller;
 
-import service.BookService;
 import service.BookServiceImpl;
 
 import javax.servlet.ServletException;
@@ -18,34 +17,63 @@ public class BookServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String endpoint = getEndpoint(req.getPathInfo());
 
+        if ("list".equals(endpoint)) { // 전체 책 리스트 조회
+            String id = getId(req.getPathInfo());
+            if (id != null) { // no에 해당하는 book상세 리턴
+                resp.getWriter().write(id);
 
-        String endPoint = parsedEndpoint(req.getPathInfo());
+            } else { // no 가 없으면 전제 리스트 조회
+                resp.getWriter().write(endpoint);
 
-        if ("list".equals(endPoint)) {
-            resp.getWriter().write("hello");
-
+            }
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String endpoint = getEndpoint(req.getPathInfo());
+
+        if ("create".equals(endpoint)) { // 책 데이터 생성 로직
+
+        }
 
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String endpoint = getEndpoint(req.getPathInfo());
+
+        if ("update".equals(endpoint)) { // 책 정보 수정 로직
+
+        }
 
 
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String endpoint = getEndpoint(req.getPathInfo());
 
+        if ("delete".equals(endpoint)) { // 책 삭제 로직
+
+        }
     }
 
-    private String parsedEndpoint(String pathInfo) {
+    private String getEndpoint(String pathInfo) {
         String[] split = pathInfo.split("/");
-        return split[1];
+        if (split.length > 1) {
+            return split[1];
+        }
+        return null;
+    }
+
+    private String getId(String pathInfo) {
+        String[] split = pathInfo.split("/");
+        if (split.length > 2) {
+            return split[2];
+        }
+        return null;
     }
 }
